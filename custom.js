472c0061430a40a7dac222c61f2700c7bd6ede63
unknown <sittsitt0007@gmail.com>
2021-06-06 01:08:47 +0630

    firstupload

---
+++ custom.js
@@ -0,0 +1,15 @@
+//Count
+function count(){
+	var shop_str=localStorage.getItem("itshop");
+	if (shop_str) {
+		var shop_arr=JSON.parse(shop_str);
+		var count=0;
+		$.each(shop_arr,function(i,v){
+			count+=v.qty;
+		})
+		$('#count').text(count);
+		// var count=shop_arr.length;
+		// console.log(count);
+		// $('#count').text(count);
+	}
+}
